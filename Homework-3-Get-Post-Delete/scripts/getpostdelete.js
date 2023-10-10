fbbf568a7b40ddadd7045fa0cceec9684d002ebb
Enes Gündüz <56505397+Deaglis1197@users.noreply.github.com>
2023-10-10 15:37:08 +0300

    Add: Get Items

--- Homework-3-Get-Post-Delete/scripts/getpostdelete.js
+++ Homework-3-Get-Post-Delete/scripts/getpostdelete.js
@@ -1,1 +1,41 @@
 console.log("Script start runing");
+
+function displayList() {
+  axios.get("https://northwind.vercel.app/api/products").then((response) => {
+    var products = response.data;
+    var count = document.getElementById("itemCount").value;
+    var counter = 0;
+    console.log(count + " " + counter);
+    products.forEach((element) => {
+      if (counter >= count) {
+        return;
+      }
+      document
+        .querySelector(".items-container")
+        .appendChild(createProductCard(element));
+      counter++;
+    });
+  });
+}
+
+function createProductCard(element) {
+  var cardElement = document.createElement("div");
+  cardElement.classList.add("card");
+
+  var h1Element = document.createElement("h1");
+  h1Element.innerText = element.id;
+  cardElement.appendChild(h1Element);
+
+  var ulElement = document.createElement("ul");
+
+  var liElement = document.createElement("li");
+  liElement.innerText = element.name;
+  ulElement.appendChild(liElement);
+
+  liElement = document.createElement("li");
+  liElement.innerText = element.unitPrice + "$";
+  ulElement.appendChild(liElement);
+
+  cardElement.appendChild(ulElement);
+  return cardElement;
+}
