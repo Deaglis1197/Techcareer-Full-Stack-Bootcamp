bb8a84b0fecd56597c60484398c10d86b223c161
Enes Gündüz <56505397+Deaglis1197@users.noreply.github.com>
2023-10-10 17:48:42 +0300

    Update: Then

--- Lesson-Homework-1/script.js
+++ Lesson-Homework-1/script.js
@@ -15,18 +15,30 @@ function customerID() {
         customerIDUL.appendChild(liElement);
         //Usa Counter
         if (element.shipAddress.country === "USA") usaCounter++;
+      });
+      document.getElementById("usaCounter").innerText += " " + usaCounter;
+      return data;
+    })
+    .then((data) => {
+      console.log("1996 order");
+      data.forEach((element) => {
         //1996 order
         var orderYear = element.orderDate.slice(0, 4);
         if (orderYear == "1996")
           console.log(
             "Customer id: " + element.customerId + " Year: " + element.orderDate
           );
+      });
+      return data;
+    })
+    .then((data) => {
+      console.log("Delayed Order");
+      data.forEach((element) => {
         //Delayed order
         requiredDate = new Date(element.requiredDate);
         shippedDate = new Date(element.shippedDate);
         if (requiredDate < shippedDate)
           console.log("Delayed Order Id: " + element.customerId);
       });
-      document.getElementById("usaCounter").innerText += " " + usaCounter;
     });
 }
